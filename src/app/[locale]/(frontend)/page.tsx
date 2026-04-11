dbd96008187811865e50279057b6073e75ffcc5b
Fausto Renier <fausto.renier@gmail.com>
2026-04-11 20:48:20 +0200

    style: :lipstick: minor

--- src/app/[locale]/(frontend)/page.tsx
+++ src/app/[locale]/(frontend)/page.tsx
@@ -1,10 +1,3 @@
-import { Link } from "@/i18n/navigation"
-
 export default async function Page() {
-  return (
-    <main className="mx-auto max-w-2xl px-4 py-10">
-      <h1 className="type-display-1 text-gray">Ducati Case</h1>
-      <Link href="/immobili">IMMOBILI</Link>
-    </main>
-  )
+  return <main className="mx-auto max-w-2xl px-4 py-10"></main>
 }
