d1a607e00919b4123213bb57dcf28e6503790edf
Fausto Renier <fausto.renier@gmail.com>
2026-05-05 21:52:02 +0200

    style: :lipstick: categories button active ui

--- src/components/listings-results/subcomponents/ListingsResultsToolbar.tsx
+++ src/components/listings-results/subcomponents/ListingsResultsToolbar.tsx
@@ -58,7 +58,7 @@ export function ListingsResultsToolbar({
           <div className="hidden lg:inline-flex lg:w-auto">
             <Button
               chevron={isCategoriesOpen ? "up" : "down"}
-              isActive={isCategoriesOpen}
+              isActive={isCategoriesOpen || selectedCategories.length > 0}
               disabled={hasSingleResultOnPage}
               onClick={() => setIsCategoriesOpen((v) => !v)}
               className="lg:w-auto"
