d2794f03dafb15141cab5bc637059268eb9970a6
Fausto Renier <fausto.renier@gmail.com>
2026-05-20 21:08:40 +0200

    feat: :sparkles: increased max file size

--- src/sanity/lib/constants.ts
+++ src/sanity/lib/constants.ts
@@ -478,7 +478,7 @@ export const ITALIAN_PROVINCE_OPTIONS = [
   { title: "Viterbo", value: "VT" },
 ] as const
 
-export const MAX_IMAGES_BYTES = 2 * 1024 * 1024
+export const MAX_IMAGES_BYTES = 5 * 1024 * 1024
 export const MAX_FLOOR_PLAN_FILE_BYTES = 3 * 1024 * 1024
 
 export const OFFICE_LAYOUT_OPTIONS = [
